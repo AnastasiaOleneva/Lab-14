7aeae9a976628e6f68748ed7328b518787147e64
Анастасия Оленёва <oleneva1995@icloud.com>
2021-11-07 14:43:02 +0300

    Добавьте файлы проекта.

---
+++ Lab 14/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cat cat = new Cat("Кошка");
+
+            Console.WriteLine(cat.Say());
+            Console.ReadKey();
+
+            Dog dog = new Dog("Собака");
+            Console.WriteLine(dog.Say());
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animals
+    {
+
+
+        public abstract string Name { get; set; }
+
+        public Animals()
+        {
+
+        }
+
+        public abstract string Say();
+
+        public string ShowInfo()
+        {
+
+            
+            return Name+ Say();
+
+           
+        }
+
+
+    }
+
+    class Cat: Animals
+    {
+        string name;
+
+        public override string Name
+        {
+            set
+            {
+                name = value;
+
+
+
+            }
+
+
+            get 
+            {
+                return name;
+            
+            
+            }
+
+        }
+
+
+       public Cat (string name) 
+        {
+            Name = name;
+        
+        
+        }
+
+
+        public override string Say()
+        {
+            
+            return name + " \t Мяу" ;
+
+        }
+    }
+
+
+    class Dog:Animals 
+    
+    {
+        string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+
+            }
+
+
+            set
+            {
+                name = value;
+
+            }
+
+        }
+        public Dog (string name)
+        {
+            Name = name;
+        }
+        public override string Say()
+        {
+
+            return name + " \t Гав";
+
+        }
+
+
+    }
+
+}
